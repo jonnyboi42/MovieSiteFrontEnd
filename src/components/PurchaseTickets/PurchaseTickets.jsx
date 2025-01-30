deb7862b008f862ac479fc3f22c19e5420f0c301
jonnyboi42 <connelljon9@gmail.com>
2025-01-30 10:03:00 -0600

    Changes

--- src/components/PurchaseTickets/PurchaseTickets.jsx
+++ src/components/PurchaseTickets/PurchaseTickets.jsx
@@ -36,10 +36,10 @@ const PurchaseTickets = () => {
         let url;
         if (movieCategory === "Coming Soon") {
           if (movieLocation.toLowerCase() === "roundrock") {
-          url = `https://moviesitebackend.onrender.com/api/comingsoon/comingsoon${movieLocation.toLowerCase()}/movie/${movieID}`;
+          url = `https://moviesitebackend.onrender.com/comingsoon/${selectedLocation.toLowerCase()}/movie/${movieID}`;
 
           } else if (movieLocation.toLowerCase() === "mueller") {
-            url = `https://moviesitebackend.onrender.com/api/comingsoon/${movieLocation.toLowerCase()}/movie/${movieID}`
+            url = `https://moviesitebackend.onrender.com/comingsoon/${selectedLocation.toLowerCase()}/movie/${movieID}`
 
           }
         } else {
